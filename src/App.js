5ec249746885886b4254d5381adc6f2f90830d3c
hojeong <hojungs87@gmail.com>
2020-09-27 15:48:42 +0900

    first commit for welfare

--- src/App.js
+++ src/App.js
@@ -45,102 +45,69 @@ function App() {
 					<TableHead>
 						<TableRow>
 							<TableCell className={classes.title} align="center" colSpan={3}>
-								생활안전교육
+								성폭력 및 아동학대 예방 교육
 							</TableCell>
 						</TableRow>
 					</TableHead>
 					<TableBody>
 						<TestRow realTime={realtime} fileIndex={0} title={'주기'} color={'#0AC073'}/>
 						<TestRow realTime={realtime} fileIndex={1} title={'내용'} color={'#0AC073'}/>
+						<TestRow realTime={realtime} fileIndex={2} title={'방법'} color={'#0AC073'}/>
 					</TableBody>
 				</Table>
 				<Table className={classes.table} style={{backgroundColor: 'rgba(250,250,210, 0.5)'}} aria-label="spanning table">
 					<TableHead>
 						<TableRow>
 							<TableCell className={classes.title} align="center" colSpan={3}>
-								교통안전교육
+								실종·유괴의 예방·방지 교육
 							</TableCell>
 						</TableRow>
 					</TableHead>
 					<TableBody>
-						<TestRow realTime={realtime} fileIndex={2} title={'주기'} color={'#0AC073'}/>
-						<TestRow realTime={realtime} fileIndex={3} title={'내용'} color={'#0AC073'}/>
+						<TestRow realTime={realtime} fileIndex={3} title={'주기'} color={'#0AC073'}/>
+						<TestRow realTime={realtime} fileIndex={4} title={'내용'} color={'#0AC073'}/>
+						<TestRow realTime={realtime} fileIndex={5} title={'방법'} color={'#0AC073'}/>
 					</TableBody>
 				</Table>
 				<Table className={classes.table} style={{backgroundColor: 'rgba(255,127,80, 0.1)'}} aria-label="spanning table">
 					<TableHead>
 						<TableRow>
 							<TableCell className={classes.title} align="center" colSpan={3}>
-								폭력예방 및 신변보호교육
+								감염병 및 약물의 오용·남용 예방 등 보건위생관리 교육
 							</TableCell>
 						</TableRow>
 					</TableHead>
 					<TableBody>
-						<TestRow realTime={realtime} fileIndex={4} title={'주기'} color={'#0AC073'}/>
-						<TestRow realTime={realtime} fileIndex={5} title={'내용'} color={'#0AC073'} />
+						<TestRow realTime={realtime} fileIndex={6} title={'주기'} color={'#0AC073'}/>
+						<TestRow realTime={realtime} fileIndex={7} title={'내용'} color={'#0AC073'} />
+						<TestRow realTime={realtime} fileIndex={8} title={'방법'} color={'#0AC073'}/>
 					</TableBody>
 				</Table>
 				<Table className={classes.table} style={{backgroundColor: 'rgba(255,20,147, 0.05)'}} aria-label="spanning table">
 					<TableHead>
 						<TableRow>
 							<TableCell className={classes.title} align="center" colSpan={3}>
-								약물 및 사이버 중독 예방 교육
+								재난대비 안전 교육
 							</TableCell>
 						</TableRow>
 					</TableHead>
 					<TableBody>
-						<TestRow realTime={realtime} fileIndex={6} title={'주기'} color={'#0AC073'}/>
-						<TestRow realTime={realtime} fileIndex={7} title={'내용'} color={'#0AC073'}/>
+						<TestRow realTime={realtime} fileIndex={9} title={'주기'} color={'#0AC073'}/>
+						<TestRow realTime={realtime} fileIndex={10} title={'내용'} color={'#0AC073'}/>
+						<TestRow realTime={realtime} fileIndex={11} title={'방법'} color={'#0AC073'}/>
 					</TableBody>
 				</Table>
 				<Table className={classes.table} style={{backgroundColor: 'rgba(123,104,238, 0.1)'}} aria-label="spanning table">
 					<TableHead>
 						<TableRow>
 							<TableCell className={classes.title} align="center" colSpan={3}>
-								재난안전교육
-							</TableCell>
-						</TableRow>
-					</TableHead>
-					<TableBody>
-						<TestRow realTime={realtime} fileIndex={8} title={'주기'} color={'#0AC073'}/>
-						<TestRow realTime={realtime} fileIndex={9} title={'내용'} color={'#0AC073'}/>
-					</TableBody>
-				</Table>
-				<Table className={classes.table} style={{backgroundColor: 'rgba(4,224,167, 0.1)'}} aria-label="spanning table">
-					<TableHead>
-						<TableRow>
-							<TableCell className={classes.title} align="center" colSpan={3}>
-								직업안전교육
-							</TableCell>
-						</TableRow>
-					</TableHead>
-					<TableBody>
-						<TestRow realTime={realtime} fileIndex={10} title={'주기'} color={'#0AC073'}/>
-						<TestRow realTime={realtime} fileIndex={11} title={'내용'} color={'#0AC073'}/>
-					</TableBody>
-				</Table>
-				<Table className={classes.table} style={{backgroundColor: 'rgba(250,250,210, 0.5)'}} aria-label="spanning table">
-					<TableHead>
-						<TableRow>
-							<TableCell className={classes.title} align="center" colSpan={3}>
-							 응급처치교육
+								교통안전 교육
 							</TableCell>
 						</TableRow>
 					</TableHead>
 					<TableBody>
 						<TestRow realTime={realtime} fileIndex={12} title={'주기'} color={'#0AC073'}/>
 						<TestRow realTime={realtime} fileIndex={13} title={'내용'} color={'#0AC073'}/>
-					</TableBody>
-				</Table>
-				<Table className={classes.table} style={{backgroundColor: 'rgba(175,238,238, 0.2)'}} aria-label="spanning table">
-					<TableHead>
-						<TableRow>
-							<TableCell className={classes.title} align="center" colSpan={3}>
-							 교육방법
-							</TableCell>
-						</TableRow>
-					</TableHead>
-					<TableBody>
 						<TestRow realTime={realtime} fileIndex={14} title={'방법'} color={'#0AC073'}/>
 					</TableBody>
 				</Table>
